ea79b848d03526fd5a01f301637ab1db194486f4
= <=>
2022-02-16 10:51:33 +0800

    fix

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/root/miniconda3/bin/python
+# -*- coding: utf-8 -*- 
+
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='nonebot_plugin_pixiv',
+    version="1.0.3",
+    description=(
+        'pixiv.net查询图片插件(支持多图和动图)'
+    ),
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author='anlen123',
+    author_email='1761512493@qq.com',
+    maintainer='anlen123',
+    maintainer_email='1761512493@qq.com',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/anlen123/nonebot_plugin_pixiv',
+    install_requires=[
+        'aiohttp',
+        'nonebot-adapter-onebot>=2.0.0-beta.1,<3.0.0',
+        'nonebot2>=2.0.0-beta.1,<3.0.0',
+        'opencv-python'
+    ]
+)
