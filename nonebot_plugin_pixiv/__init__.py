48f00b462e677ed4d67447f3fde57b220781f847
anlen123 <1761512493@qq.com>
2026-08-02 15:39:52 +0800

    适配win

--- nonebot_plugin_pixiv/__init__.py
+++ nonebot_plugin_pixiv/__init__.py
@@ -5,6 +5,8 @@ from nonebot.plugin import on_message, on_regex
 from nonebot.adapters.onebot.v11 import Bot, Event, Message, MessageSegment, GroupMessageEvent
 import aiohttp, re, os, random, cv2, asyncio, base64
 
+from PIL import Image
+
 from nonebot import require
 
 require("nonebot_plugin_apscheduler")
@@ -15,10 +17,8 @@ imgRoot = config.get('imgroot') if config.get('imgroot') else f"{os.environ['HOM
 proxy_aiohttp = config.get('aiohttp') if config.get('aiohttp') else ""
 pixiv_cookies = config.get('pixiv_cookies') if config.get('pixiv_cookies') else ""
 ffmpeg = config.get('ffmpeg') if config.get('ffmpeg') else "/usr/bin/ffmpeg"
-headersCook = {
-    'referer': 'https://www.pixiv.net',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-}
+headersCook = {'referer': 'https://www.pixiv.net',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36', }
 if pixiv_cookies:
     headersCook['cookie'] = pixiv_cookies
 
@@ -41,11 +41,11 @@ elif PIXIV_R18:
 BAN_PIXIV_R18 = eval(config.get('ban_pixiv_r18', '[]'))
 BAN_PIXIV_R18 = [int(_) for _ in BAN_PIXIV_R18]
 
-pathHome = f"{imgRoot}QQbotFiles/pixiv"
+pathHome = f"{imgRoot}QQbotFiles\pixiv"
 if not os.path.exists(pathHome):
     os.makedirs(pathHome)
 
-pathZipHome = f"{imgRoot}QQbotFiles/pixivZip"
+pathZipHome = f"{imgRoot}QQbotFiles\pixivZip"
 if not os.path.exists(pathZipHome):
     os.makedirs(pathZipHome)
 
@@ -126,7 +126,7 @@ async def fetch(session, url, name):
         code = response.status
         if code == 200:
             content = await response.content.read()
-            with open(f"{imgRoot}QQbotFiles/pixiv/" + name, mode='wb') as f:
+            with open(f"{imgRoot}QQbotFiles\pixiv\\" + name, mode='wb') as f:
                 f.write(content)
             return True
         return False
@@ -144,7 +144,7 @@ async def main(PID):
 
         if not url:
             print("官方api没找到尝试用第三方api")
-            resp2 = await session.get(url=f"https://api.obfs.dev/api/pixiv/illust?id={PID}", headers=headersCook,
+            resp2 = await session.get(url=f"https://api.obfs.dev/api/pixiv\\illust?id={PID}", headers=headersCook,
                                       proxy=proxy_aiohttp)
             cc = await resp2.json()
             if cc.get('error'):
@@ -162,8 +162,8 @@ async def main(PID):
         suffix = name.split(".")[1]
         names = []
         num = 0
-        if os.path.exists(f"{imgRoot}QQbotFiles/pixiv/" + name):
-            while os.path.exists(f"{imgRoot}QQbotFiles/pixiv/" + name) and num <= 6:
+        if os.path.exists(f"{imgRoot}QQbotFiles\pixiv\\" + name):
+            while os.path.exists(f"{imgRoot}QQbotFiles\pixiv\\" + name) and num <= 6:
                 names.append(name)
                 num += 1
                 name = re.sub("_p(\d+)\.(png|jpg|jepg)", f"_p{num}.{suffix}", name)
@@ -194,11 +194,7 @@ pixivRank = on_regex(pattern="^pixivRank\ ")
 @pixivRank.handle()
 async def pixiv_rev(bot: Bot, event: Event):
     info = str(event.get_plaintext()).strip()[10:].strip()
-    dic = {
-        "1": "day",
-        "7": "weekly",
-        "30": "monthly"
-    }
+    dic = {"1": "day", "7": "weekly", "30": "monthly"}
     if info in dic.keys():
         img_list = random.choices(await get_Img_ByDay(dic[info]), k=5)
         names = []
@@ -211,7 +207,7 @@ async def pixiv_rev(bot: Bot, event: Event):
             for name in names:
                 if name:
                     for t in name:
-                        path = f"{imgRoot}QQbotFiles/pixiv/{t}"
+                        path = f"{imgRoot}QQbotFiles\pixiv\\{t}"
                         size = os.path.getsize(path)
                         if size // 1024 // 1024 >= 10:
                             await ya_suo(path)
@@ -230,6 +226,7 @@ async def pixiv_rev(bot: Bot, event: Event):
 
 async def base64_path(path: str):
     ff = "空"
+    print(path)
     with open(path, "rb") as f:
         ff = base64.b64encode(f.read()).decode()
     return f"base64://{ff}"
@@ -242,35 +239,49 @@ async def send(PID: str, event: Event, bot: Bot):
     else:
         msg = None
         for name in names:
-            path = f"{imgRoot}QQbotFiles/pixiv/{name}"
+            path = f"{imgRoot}QQbotFiles\pixiv\\{name}"
             size = os.path.getsize(path)
             if size // 1024 // 1024 >= 10:
                 await ya_suo(path)
             msg += MessageSegment.image(await base64_path(path))
+        flag = False
         try:
             if isinstance(event, GroupMessageEvent):
+                print("1")
                 await send_forward_msg_group(bot, event, 'qqbot', msg)
+                print(event.user_id)
+                await bot.send_private_msg(user_id=event.user_id, message=msg)
             else:
+                print("2")
                 await bot.send(event=event, message=msg)
-
-        except:
+            print("5")
+        except Exception as ee:
+            print(ee)            
             try:
                 for name in names:
-                    path = f"{imgRoot}QQbotFiles/pixiv/{name}"
+                    path = f"{imgRoot}QQbotFiles\pixiv\\{name}"
                     os.system(f"echo '1' >> {path}")
                 msg = None
                 for name in names:
-                    path = f"{imgRoot}QQbotFiles/pixiv/{name}"
+                    path = f"{imgRoot}QQbotFiles\pixiv\\{name}"
                     size = os.path.getsize(path)
                     if size // 1024 // 1024 >= 10:
                         await ya_suo(path)
                     msg += MessageSegment.image(await base64_path(path))
                 if isinstance(event, GroupMessageEvent):
+                    print("3")
                     await send_forward_msg_group(bot, event, 'qqbot', msg)
+                    flag = True
                 else:
+                    print("4")
                     await bot.send(event=event, message=msg)
+                    flag = True
+                
             except:
                 await bot.send(event=event, message="查询失败, 帐号有可能发生风控，请检查!!!")
+                
+            if not flag:
+                await bot.send(event=event, message=msg)
 
 
 # 压缩图片大小
@@ -296,21 +307,9 @@ async def check_GIF(PID: str) -> str:
 
 
 async def GIF_send(url: str, PID: str, event: Event, bot: Bot):
-    path_pre = f"{imgRoot}QQbotFiles/pixivZip/{PID}"
-    if os.path.exists(f"{path_pre}/{PID}.gif"):
-        size = os.path.getsize(f"{path_pre}/{PID}.gif")
-        while size // 1024 // 1024 >= 15:
-            msg = await run(f"file {path_pre}/{PID}.gif")
-            chang = int(msg.split(" ")[-3]) // 2
-            kuan = int(msg.split(" ")[-1]) // 2
-            await run(f"{ffmpeg} -i {path_pre}/{PID}.gif -s {chang}x{kuan} {path_pre}/{PID}_temp.gif")
-            await run(f"rm -rf {path_pre}/{PID}.gif")
-            await run(f"mv {path_pre}/{PID}_temp.gif {path_pre}/{PID}.gif")
-            size = os.path.getsize(f"{path_pre}/{PID}.gif")
-        try:
-            await bot.send(event=event, message=MessageSegment.image(await base64_path(f"{path_pre}/{PID}.gif")))
-        except:
-            await bot.send(event=event, message="查询失败, 帐号有可能发生风控，请检查")
+    path_pre = f"{imgRoot}QQbotFiles\pixivZip\{PID}"
+    if os.path.exists(f"{path_pre}\{PID}.gif"):
+        await send_big_img(path_pre, PID, event, bot)
         return
     async with aiohttp.ClientSession() as session:
         response = await session.get(url=url, headers=headersCook, proxy=proxy_aiohttp)
@@ -324,49 +323,40 @@ async def GIF_send(url: str, PID: str, event: Event, bot: Bot):
                     os.mkdir(f"{path_pre}")
             await run(f"unzip -n {path_pre}.zip -d {path_pre}")
             image_list = sorted(os.listdir(f"{path_pre}"))
-            await run(f"rm -rf {path_pre}.zip")
-            await run(f"{ffmpeg} -r {len(image_list)} -i {path_pre}/%06d.jpg {path_pre}/{PID}.gif -n")
+            await run(f"del {path_pre}.zip")
+            print(f"{ffmpeg} -r {len(image_list)} -i {path_pre}\%06d.jpg -pix_fmt rgb24  {path_pre}\{PID}.gif -n")
+            await run(f"{ffmpeg} -r {len(image_list)} -i {path_pre}\%06d.jpg -pix_fmt rgb24 {path_pre}\{PID}.gif -n")
             # 压缩
-            size = os.path.getsize(f"{path_pre}/{PID}.gif")
-            while size // 1024 // 1024 >= 15:
-                msg = await run(f"file {path_pre}/{PID}.gif")
-                chang = int(msg.split(" ")[-3]) // 2
-                kuan = int(msg.split(" ")[-1]) // 2
-                await run(f"{ffmpeg} -i {path_pre}/{PID}.gif -s {chang}x{kuan} {path_pre}/{PID}_temp.gif")
-                await run(f"rm -rf {path_pre}/{PID}.gif")
-                await run(f"mv {path_pre}/{PID}_temp.gif {path_pre}/{PID}.gif")
-                size = os.path.getsize(f"{path_pre}/{PID}.gif")
-            try:
-                await bot.send(event=event, message=MessageSegment.image(await base64_path(f"{path_pre}/{PID}.gif")))
-            except:
-                await bot.send(event=event, message="查询失败, 帐号有可能发生风控，请检查")
+            await send_big_img(path_pre, PID, event, bot)
+
+
+async def send_big_img(path_pre: str, PID: str, event: Event, bot: Bot):
+    size = os.path.getsize(f"{path_pre}\{PID}.gif")
+    while size // 1024 // 1024 >= 15:
+        await bot.send(event=event, message="图片太大不给发, 随便来一张吧")
+        await bot.send(event=event, message=MessageSegment.image(await base64_path(f"{path_pre}\\000000.jpg")))
+        return
+    try:
+        await bot.send(event=event, message=MessageSegment.image(await base64_path(f"{path_pre}\{PID}.gif")))
+    except:
+        await bot.send(event=event, message="查询失败, 帐号有可能发生风控，请检查")
+    return
 
 
 async def run(cmd: str):
-    print(cmd)
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
+    os.system(
+        cmd)  # print(cmd)  # proc = await asyncio.create_subprocess_shell(  #     cmd,  #     stdout=asyncio.subprocess.PIPE,  #     stderr=asyncio.subprocess.PIPE)
 
-    stdout, stderr = await proc.communicate()
-    return (stdout + stderr).decode()
+    # stdout, stderr = await proc.communicate()  # return (stdout + stderr).decode()
 
 
 # 合并消息
-async def send_forward_msg_group(
-        bot: Bot,
-        event: GroupMessageEvent,
-        name: str,
-        msgs: List[str],
-):
+async def send_forward_msg_group(bot: Bot, event: GroupMessageEvent, name: str, msgs: List[str], ):
     def to_json(msg):
         return {"type": "node", "data": {"name": name, "uin": bot.self_id, "content": msg}}
 
     messages = [to_json(msg) for msg in msgs]
-    await bot.call_api(
-        "send_group_forward_msg", group_id=event.group_id, messages=messages
-    )
+    await bot.call_api("send_group_forward_msg", group_id=event.group_id, messages=messages)
 
 
 async def pan_R18(PID) -> bool:
